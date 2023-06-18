dad18f730521ed8eeac26b08f5a729a9d059c8ee
Jocilé Serra <jocile@gmail.com>
2023-06-18 18:31:49 -0300

    maven: configuração de criação do executável jar

---
+++ escola.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Marcar este arquivo como executável no linux
+# Ao compilar o programa com o Maven o executável .jar será criado na pasta target
+java -jar target/escola-1.0-SNAPSHOT.jar
